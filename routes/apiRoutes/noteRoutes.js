5f8dd6b7d9e52a52308dd62dda9ae95dfc8eaa74
JCIII <jcostello711@gmail.com>
2022-03-19 18:43:23 -0400

    clicking the save icon will add the note to the left column and db.json

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,9 +1,23 @@
 const {} = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = req.body;
+    notes.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify({ notes }, null, 2)
+    );
+    res.json(notes);
+})
 
 module.exports  = router;
